f7fcf5131f1a2f8ae005d9ebc96273ce20f741f7
YounesRabeh <rabehyounes9@gmail.com>
2026-02-27 16:03:35 +0100

    [DOC] lte

--- components/lte/lte.h
+++ components/lte/lte.h
@@ -3,6 +3,17 @@
 #include "esp_err.h"
 #include <stdbool.h>
 
+/**
+ * @brief Inizializza il modem LTE
+ */
 esp_err_t lte_init(void);
+
+/**
+ * @brief Connette alla rete LTE
+ */
 esp_err_t lte_connect(void);
+
+/**
+ * @brief Restituisce true se la rete LTE è connessa
+ */
 bool lte_is_connected(void);
